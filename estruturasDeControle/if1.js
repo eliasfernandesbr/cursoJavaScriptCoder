40cff2a9d9050da7ccf0755a80466d431d30e698
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2020-10-25 09:29:31 -0300

    if

---
+++ estruturasDeControle/if1.js
@@ -0,0 +1,28 @@
+function soBoaNoticia(nota){
+    if(nota >= 7){
+        console.log("APROVADO COM " + nota)
+
+    }
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(6.1)
+
+function seForVerdadeEuFalo(valor){
+    if(valor){
+        console.log("É VERDADE... " + valor)
+    }
+}
+
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo("")
+seForVerdadeEuFalo(0)
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(" ")
+seForVerdadeEuFalo("?")
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1, 21])
+seForVerdadeEuFalo({})
